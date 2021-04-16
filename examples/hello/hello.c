c8dc547946ac231f56517ef950b00bd101957cc7
bigmagic <fujin.zhao@foxmail.com>
2021-04-17 00:19:06 +0800

    add origin iar

--- examples/hello/hello.c
+++ examples/hello/hello.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
 
+static int custom_cube(int addr)
+{
+    int cube;
+    
+    asm volatile (
+       ".insn r 0x7b, 6, 6, %0, %1, x0"
+             :"=r"(cube)
+             :"r"(addr)
+     );
+    
+    return cube; 
+}
+
 void main()
 {
-    const char *s = "Hello.\n";
-    while (*s) putchar(*s++);
+    int a = 3;
+    int ret = 0;
+    ret = custom_cube((int)&a);
+    if(ret == a*a*a)
+    {
+        putchar('o');
+        putchar('k');
+    }
+    else
+    {
+        putchar('e');
+        putchar('r');
+        putchar('r');
+    }
+
     while(1);
 }
